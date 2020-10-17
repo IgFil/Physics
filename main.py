097cb28165ffa4714f342778c1e429b4b8ecabd2
IgFil <38530968+IgFil@users.noreply.github.com>
2020-10-17 10:12:52 +0300

    VERSION BETA 1.0.
    STABLE PROGRAM

---
+++ main.py
@@ -0,0 +1,47 @@
+from colorama import Fore, init
+import subprocess
+import difflib
+import sqlite3
+import os.path
+i = 0
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "formul.db")
+init(autoreset=True)
+
+def connect_db():
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    return conn, cursor
+def similarity(s1, s2):
+    normalized1 = s1.lower()
+    normalized2 = s2.lower()
+    matcher = difflib.SequenceMatcher(None, normalized1, normalized2)
+    return matcher.ratio()
+print("""█▀ ▄▀▄ █▀▀▄ █▄░▄█ █░█ █░░     █▀▄ █░░ ▀▄░▄▀ ▄▀▀ ▀ ▄▀ ▄▀▀ 
+█▀ █░█ █▐█▀ █░█░█ █░█ █░▄     █░█ █▀▄ ░░█░░ ░▀▄ █ █░ ░▀▄ 
+▀░ ░▀░ ▀░▀▀ ▀░░░▀ ░▀░ ▀▀▀     █▀░ ▀░▀ ░░▀░░ ▀▀░ ▀ ░▀ ▀▀░ 
+
+Создатель: © Игнат Филиппов Юрьевич. 
+Примечание:
+v - скорость
+V - объём
+""")
+update = input("Хотите ли вы обновить базу данных с формулами?(у/n")
+if (update == "y"):
+    subprocess_update = subprocess.run(["git pull", ""])
+while True:
+    data = input(Fore.YELLOW + "Введите данные задачи через пробел.\n")
+    conn , cursor = connect_db()
+    cursor.execute("SELECT formul FROM formuls;")
+    formuls = cursor.fetchall()
+    chances = []
+    for formula in formuls:
+        chances.append(similarity(data, formula[0]))
+    for chance in chances: 
+        if chance >= 0.50 :
+                chance_percent = chance * 100;
+                chance_percent = int(chance_percent)
+                print (Fore.GREEN + formuls[i][0] + "  " + str(chance_percent) +"%")
+        i = i + 1   
+    i = 0
+    
